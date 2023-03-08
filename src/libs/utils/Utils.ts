ae4bcd799363689ab29ca29f3c97e948d0e51cae
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-03-08 12:07:12 +0100

    Update Utils.ts
    - Update doc
    - Add uuid_e4

--- src/libs/utils/Utils.ts
+++ src/libs/utils/Utils.ts
@@ -1,9 +1,35 @@
+/**
+ * Formats a SIREN number.
+ *
+ * @param str The string to format.
+ * @returns The formatted string or `undefined` if the input is not valid.
+ */
 export const formatSiren = (str?: string): string | undefined => {
     if (!str || str.length !== 9) return str
     return `${str.slice(0, 3)} ${str.slice(3, 6)} ${str.slice(6, 9)}`
 }
 
+/**
+ * Formats a SIRET number.
+ *
+ * @param str The string to format.
+ * @returns The formatted string or `undefined` if the input is not valid.
+ */
 export const formatSiret = (str?: string): string | undefined => {
     if (!str || str.length !== 14) return str
     return `${str.slice(0, 3)} ${str.slice(3, 6)} ${str.slice(6, 9)} ${str.slice(9, 12)} ${str.slice(12)}`
+}
+
+/**
+ * Generate a version 4 UUID.
+ * @returns A version 4 UUID.
+ */
+export function uuid_e4(): string {
+    const h = '0123456789abcdef';
+    let u = '';
+    for (let i = 0; i < 36; i++) {
+        const c = i === 8 || i === 13 || i === 18 || i === 23 ? '-' : i === 14 ? '4' : 'x';
+        u += c === '-' || c === '4' ? c : h[Math.floor(Math.random() * 16)];
+    }
+    return u;
 }
