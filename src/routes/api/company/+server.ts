0ba6e94facf3f2eff0f6d68b032205ce166f3635
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-03-08 12:09:22 +0100

    Fixtures imports

--- src/routes/api/company/+server.ts
+++ src/routes/api/company/+server.ts
@@ -1,6 +1,6 @@
 import {dev} from '$app/environment';
 import {error} from '@sveltejs/kit';
-import {COMPANY_API_URL, type IApe, type ICj, type ICompany} from '../../../libs/company/Company';
+import {COMPANY_API_URL, type IApe, type ICj, type ICompany} from '$libs/company/Company';
 
 /** @type {import('./$types').RequestHandler} */
 export async function GET({url, fetch}: { url: URL, fetch: any }) {
