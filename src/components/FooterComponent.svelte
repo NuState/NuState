4e11388834f6c5351c601ded3e424f0b34a6c81c
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-03-08 12:45:27 +0100

    Fixtures Form component & Tables Company route page

--- src/components/FooterComponent.svelte
+++ src/components/FooterComponent.svelte
@@ -2,13 +2,13 @@
     import {Footer, FooterCopyright, FooterLink, FooterLinkGroup, Indicator} from "flowbite-svelte";
 </script>
 
-<section class="mx-auto w-1/2 drop-shadow-md">
+<section class="mx-auto w-3/4 lg:w-1/2 drop-shadow-md">
     <article class="my-2 w-full">
-        <Footer class="!bg-transparent">
+        <Footer class="!bg-transparent text-center">
             <FooterCopyright by="NuState™" href="https://github.com/NuState/NuStateAPI"
                              year={new Date().getFullYear()}/>
             <FooterLinkGroup
-                    ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+                    ulClass="flex flex-wrap justify-center items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
                 <FooterLink href="https://github.com/NuState/NuStateAPI">Github</FooterLink>
                 <FooterLink href="/"><span class="flex inline-flex">Status<Indicator color="red" size="xs"/></span>
                 </FooterLink>
--- src/routes/company/+page.svelte
+++ src/routes/company/+page.svelte
@@ -51,35 +51,39 @@
 <section class="relative mx-6 lg:mx-24 mt-14 mb-3 transition-all duration-300 ease-in-out">
     <article class="flex flex-col items-center justify-center space-y-3">
         <form autocomplete="off" class="w-full" on:submit|preventDefault={getFilteredCompanies}>
-            <Search bind:value={value} class="transition-all duration-300 ease-in-out shadow"
+            <Search bind:value={value} class="transition-all duration-300 ease-in-out shadow dark:!bg-transparent"
                     on:input={getFilteredCompanies}>
                 <Button class="drop-shadow-md shadow transition-all duration-300 ease-in-out">Rechercher</Button>
             </Search>
         </form>
         {#if value && value.length >= 3 && companies.length > 0}
-            <Table class="transition-all duration-300 ease-in-out" shadow>
-                <TableHead>
-                    <TableHeadCell>SIRET</TableHeadCell>
-                    <TableHeadCell>Dénominations</TableHeadCell>
-                    <TableHeadCell>Activité principale exercée</TableHeadCell>
-                    <TableHeadCell></TableHeadCell>
-                </TableHead>
-                <TableBody class="divide-y">
-                    {#each companies as company}
-                        <TableBodyRow>
-                            <TableBodyCell>{formatSiret(company?.siege?.siret)}</TableBodyCell>
-                            <TableBodyCell>{company?.nom_complet}</TableBodyCell>
-                            <TableBodyCell>{company?.libelle_activite_principale_n5}</TableBodyCell>
-                            <TableBodyCell>
-                                <Button class="drop-shadow-md shadow transition-all duration-300 ease-in-out"
-                                        href="/company/{company?.siege?.siret}">Continuer
-                                    <ArrowRightSvg className="h-4 w-4"></ArrowRightSvg>
-                                </Button>
-                            </TableBodyCell>
-                        </TableBodyRow>
-                    {/each}
-                </TableBody>
-            </Table>
+            <section class="overflow-x-auto w-screen px-3 sm:px-6 lg:px-9 py-2">
+                <article class="relative w-full flex items-center justify-center">
+                    <Table class="transition-all duration-300 ease-in-out" shadow>
+                        <TableHead>
+                            <TableHeadCell>SIRET</TableHeadCell>
+                            <TableHeadCell>Dénominations</TableHeadCell>
+                            <TableHeadCell>Activité principale exercée</TableHeadCell>
+                            <TableHeadCell></TableHeadCell>
+                        </TableHead>
+                        <TableBody class="divide-y">
+                            {#each companies as company}
+                                <TableBodyRow>
+                                    <TableBodyCell>{formatSiret(company?.siege?.siret)}</TableBodyCell>
+                                    <TableBodyCell>{company?.nom_complet}</TableBodyCell>
+                                    <TableBodyCell>{company?.libelle_activite_principale_n5}</TableBodyCell>
+                                    <TableBodyCell>
+                                        <Button class="drop-shadow-md shadow transition-all duration-300 ease-in-out"
+                                                href="/company/{company?.siege?.siret}">Continuer
+                                            <ArrowRightSvg className="h-4 w-4"></ArrowRightSvg>
+                                        </Button>
+                                    </TableBodyCell>
+                                </TableBodyRow>
+                            {/each}
+                        </TableBody>
+                    </Table>
+                </article>
+            </section>
         {:else if value && value.length >= 3 && buffer}
             <Spinner class="drop-shadow-md m-6" size={8}></Spinner>
         {:else if value && value.length >= 3}
