f66ecc2541c778c606cde8d1b3228463524ee69c
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-03-08 09:51:32 +0100

    Update Utils.ts

--- src/libs/utils/Utils.ts
+++ src/libs/utils/Utils.ts
@@ -1,4 +1,9 @@
 export const formatSiren = (str?: string): string | undefined => {
     if (!str || str.length !== 9) return str
     return `${str.slice(0, 3)} ${str.slice(3, 6)} ${str.slice(6, 9)}`
-};
+}
+
+export const formatSiret = (str?: string): string | undefined => {
+    if (!str || str.length !== 14) return str
+    return `${str.slice(0, 3)} ${str.slice(3, 6)} ${str.slice(6, 9)} ${str.slice(9, 12)} ${str.slice(12)}`
+}
