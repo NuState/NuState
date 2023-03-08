6bf88602370b68370c424126417659ae2e41a470
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-03-08 12:06:49 +0100

    Add environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+// noinspection SpellCheckingInspection
+
+export const environment = {
+    firebaseConfig: {
+        apiKey: 'AIzaSyAtl8J1h5cL7F3S_Zhr8feGsMK5_0Twn5Y',
+        authDomain: 'nustate-c341b.firebaseapp.com',
+        databaseURL: 'https://nustate-c341b-default-rtdb.europe-west1.firebasedatabase.app',
+        projectId: 'nustate-c341b',
+        storageBucket: 'nustate-c341b.appspot.com',
+        messagingSenderId: '179535979291',
+        appId: '1:179535979291:web:9b2770c3e856a00c8e29be',
+        measurementId: 'G-SNH7B6N4LZ'
+    }
+}
