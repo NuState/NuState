e8c58b4d8368f9478e083444c8ed77d555e62366
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-03-10 17:08:52 +0100

    Remove edge function, fix package

--- src/routes/json1/+server.ts
+++ src/routes/json1/+server.ts
@@ -1,5 +1,5 @@
 import {Feed, type Item} from "feed";
-import admin from "firebase-admin/app";
+import * as admin from "firebase-admin";
 import {environment} from "../../environments/environment-server";
 import {dev} from "$app/environment";
 import {environmentDev} from "../../environments/environment-dev-server";
@@ -8,23 +8,23 @@ import {getDatabase} from "firebase-admin/database";
 /** @type {import('../../../.svelte-kit/types/src/routes').RequestHandler} */
 export async function GET({url}: { url: URL }) {
 
-    let firebaseApp: admin.App | undefined
+    let firebaseApp: admin.app.App | undefined
     try {
-        firebaseApp = admin.getApp('[ADMIN_DEFAULT]')
+        firebaseApp = admin.app('[ADMIN_DEFAULT]')
     } catch (reason) {
         if (dev) console.log(reason)
     }
 
-    if (firebaseApp) await admin.deleteApp(firebaseApp)
+    if (firebaseApp) await firebaseApp.delete()
 
     if (dev) {
         firebaseApp = admin.initializeApp({
-            credential: admin.cert(environmentDev.firebaseConfig),
+            credential: admin.credential.cert(environmentDev.firebaseConfig),
             databaseURL: environmentDev.firebaseDatabaseURL
         }, '[ADMIN_DEFAULT]')
     } else {
         firebaseApp = admin.initializeApp({
-            credential: admin.cert(environment.firebaseConfig),
+            credential: admin.credential.cert(environment.firebaseConfig),
             databaseURL: environment.firebaseDatabaseURL
         }, '[ADMIN_DEFAULT]')
     }
@@ -60,7 +60,7 @@ export async function GET({url}: { url: URL }) {
         rss.addItem(_item)
     }
 
-    await admin.deleteApp(firebaseApp)
+    await firebaseApp.delete()
 
     return new Response(rss.json1(), {
         headers: {
