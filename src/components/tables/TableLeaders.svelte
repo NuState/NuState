2f585a58d48b23cc2779765749ad10af94ce013e
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-03-30 08:31:06 +0200

    Update TableLeaders component

--- src/components/tables/TableLeaders.svelte
+++ src/components/tables/TableLeaders.svelte
@@ -2,6 +2,7 @@
     import {Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell} from "flowbite-svelte";
     import {isPhysicalPerson} from "french-company-types";
     import {formatSiren} from "$libs/public-api";
+    import {PopoverSIREN, SubCategory} from "$components/public-api";
 
     export let leaders
 </script>
@@ -16,22 +17,73 @@
             <TableHeadCell>SIREN</TableHeadCell>
         </TableHead>
         <TableBody>
-            {#each leaders as leader}
+            {#each leaders as leader, i}
                 <TableBodyRow>
-                    <TableBodyCell>{leader?.type_dirigeant?.split(' ')?.map(e => e.at(0))?.join('')?.toUpperCase()}</TableBodyCell>
+                    <TableBodyCell>
+                        <SubCategory className="flex inline-flex items-center">
+                            <p>{leader?.type_dirigeant?.split(' ')?.map(e => e.at(0))?.join('')?.toUpperCase()}</p>
+                        </SubCategory>
+                    </TableBodyCell>
                     {#if isPhysicalPerson(leader)}
-                        <TableBodyCell>{leader?.nom ?? 'N/A'} {leader?.prenoms?.toUpperCase() ?? 'N/A'}</TableBodyCell>
-                        <TableBodyCell>{leader?.qualite ?? 'N/A'}</TableBodyCell>
-                        <TableBodyCell>{leader?.annee_de_naissance ?? 'N/A'}</TableBodyCell>
-                        <TableBodyCell>/</TableBodyCell>
+                        <TableBodyCell>
+                            <SubCategory className="flex inline-flex items-center"
+                                         indicatorColor={leader?.prenoms ? 'green': 'red'}>
+                                <p>{leader?.nom ?? 'N/A'} {leader?.prenoms?.toUpperCase() ?? 'N/A'}</p>
+                            </SubCategory>
+                        </TableBodyCell>
+                        <TableBodyCell>
+                            <SubCategory className="flex inline-flex items-center"
+                                         indicatorColor={leader?.qualite ? 'green': 'red'}>
+                                <p>{leader?.qualite ?? 'N/A'}</p>
+                            </SubCategory>
+                        </TableBodyCell>
+                        <TableBodyCell>
+                            <SubCategory className="flex inline-flex items-center"
+                                         indicatorColor={leader?.annee_de_naissance ? 'green': 'red'}>
+                                <p>{leader?.annee_de_naissance ?? 'N/A'}</p>
+                            </SubCategory>
+                        </TableBodyCell>
+                        <TableBodyCell>
+                            <SubCategory className="flex inline-flex items-center">
+                                <p>/</p>
+                            </SubCategory>
+                        </TableBodyCell>
                     {:else}
-                        <TableBodyCell>{leader?.denomination ?? 'N/A'}</TableBodyCell>
-                        <TableBodyCell>{leader?.qualite ?? 'N/A'}</TableBodyCell>
-                        <TableBodyCell>/</TableBodyCell>
-                        <TableBodyCell>{formatSiren(leader?.siren) ?? 'N/A'}</TableBodyCell>
+                        <TableBodyCell>
+                            <SubCategory className="flex inline-flex items-center"
+                                         indicatorColor={leader?.denomination ? 'green': 'red'}>
+                                <p>{leader?.denomination ?? 'N/A'}</p>
+                            </SubCategory>
+                        </TableBodyCell>
+                        <TableBodyCell>
+                            <SubCategory className="flex inline-flex items-center"
+                                         indicatorColor={leader?.qualite ? 'green': 'red'}>
+                                <p>{leader?.qualite ?? 'N/A'}</p>
+                            </SubCategory>
+                        </TableBodyCell>
+                        <TableBodyCell>
+                            <SubCategory className="flex inline-flex items-center">
+                                <p>/</p>
+                            </SubCategory>
+                        </TableBodyCell>
+                        <TableBodyCell>
+                            {#if !leader?.denomination?.toUpperCase?.()?.includes?.('L\'ETAT')}
+                                <SubCategory className="flex inline-flex items-center" hasInfoIcon={true}
+                                             indicatorColor={leader?.siren ? 'green': 'red'} pId={`siren_leader_${i}`}>
+                                    <p>{formatSiren(leader?.siren) ?? 'N/A'}</p>
+                                </SubCategory>
+                            {:else if leader?.denomination?.toUpperCase?.()?.includes?.('L\'ETAT')}
+                                <SubCategory className="flex inline-flex items-center">
+                                    <p>/</p>
+                                </SubCategory>
+                            {/if}
+                        </TableBodyCell>
                     {/if}
                 </TableBodyRow>
             {/each}
         </TableBody>
     </Table>
+    {#each leaders as _, i}
+        <PopoverSIREN placement="top" triggeredBy="{`#siren_leader_${i}`}"></PopoverSIREN>
+    {/each}
 {/if}
