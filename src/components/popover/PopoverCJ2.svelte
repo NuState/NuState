24faf09e01ea9590beb9a2ce37138bd6bc274e7b
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-03-30 08:28:49 +0200

    Update PopoverCJ2

--- src/components/popover/PopoverCJ2.svelte
+++ src/components/popover/PopoverCJ2.svelte
@@ -3,9 +3,10 @@
     import {slide} from "svelte/transition";
 
     export let triggeredBy
+    export let placement = undefined
 </script>
 
-<Popover class="w-64 text-sm font-light" placement="top" title="Catégorie juridique" transition={slide}
+<Popover class="w-64 text-sm font-light" placement={placement ?? 'top'} title="Catégorie juridique" transition={slide}
          trigger="hover" triggeredBy={triggeredBy}>
     <span class="font-bold">Niveau II</span> : 37 positions définissant des catégories juridiques par rapport aux
     critères juridiques fondamentaux du droit.
