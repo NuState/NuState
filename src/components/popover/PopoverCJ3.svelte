4c41b919b0c580f8766f9cdc1760db65de25f81c
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-03-30 08:28:58 +0200

    Update PopoverCJ3

--- src/components/popover/PopoverCJ3.svelte
+++ src/components/popover/PopoverCJ3.svelte
@@ -3,9 +3,10 @@
     import {slide} from "svelte/transition";
 
     export let triggeredBy
+    export let placement = undefined
 </script>
 
-<Popover class="w-64 text-sm font-light" placement="top" title="Catégorie juridique" transition={slide}
+<Popover class="w-64 text-sm font-light" placement={placement ?? 'top'} title="Catégorie juridique" transition={slide}
          trigger="hover" triggeredBy={triggeredBy}>
     <span class="font-bold">Niveau III</span> : 260 positions permettant de préciser la catégorie juridique en tenant
     compte de la spécificité des dispositions juridiques concernant les unités concernées.
