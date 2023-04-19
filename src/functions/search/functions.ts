95a8a7bd23082e3a20fbd1f19d981ef540abdac3
Wesley LEVASSEUR <mailpro.levasseur@icloud.com>
2023-04-19 07:30:30 +0200

    Add Search functions

---
+++ src/functions/search/functions.ts
@@ -0,0 +1,9 @@
+export const searchCodes = [
+    {value: 'Ent', name: 'Entreprise'},
+    {value: 'Étab', name: 'Établissement'},
+] as const
+
+export const searchCodesMap = [
+    {code: 'Ent', details: 'company', filtered: 'companies'},
+    {code: 'Étab', details: 'establishment', filtered: 'establishments'},
+] as const
